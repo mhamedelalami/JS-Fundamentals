4a6b9d8ae8ec9b1957c8877fd2d0e632ec3eb23f
M'hamed El Alami <elalamimhamed00@gmail.com>
2025-05-23 22:38:47 +0100

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for(let i = 0; i < lines.length; i++){
-    console.log(lines[i]);
+let i = 0;
+while (i < lines.length) {
+  console.log(lines[i]);
+  i++;
 }
