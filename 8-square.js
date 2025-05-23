fba77cd6d35d47c07c9adeb80a0eac911f982744
M'hamed El Alami <elalamimhamed00@gmail.com>
2025-05-23 23:16:26 +0100

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -3,7 +3,9 @@ const size = parseInt(process.argv[2]);
 if(isNaN(size)){
     console.log('Missing size')
 } else {
-for(let i = 0; i < size; i++) {
-   console.log('X'.repeat(size));
- }
+  let i = 0;
+  while(i < size){
+    console.log('X'.repeat(size));
+    i++;
+}
 }
